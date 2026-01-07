0aa3aac5582a4e96859c0199348d884b24838630
yujung23 <kimyj4017@gmail.com>
2026-01-07 14:56:38 +0900

    feat: css 연결 및 수정

--- public/script.js
+++ public/script.js
@@ -7,14 +7,48 @@ function renderMessage(text, sender) {
     const messageDiv = document.createElement('div');
     messageDiv.className = `message ${sender}`;
 
+    if (sender === 'bot'){
+        const logoImg = document.createElement('img');
+        logoImg.src = 'bot-logo.png'; 
+        logoImg.className = 'bot-logo';
+        messageDiv.appendChild(logoImg);
+    }
+
     const bubbleDiv = document.createElement('div');
     bubbleDiv.className = 'bubble';
     bubbleDiv.textContent = text;
 
     messageDiv.appendChild(bubbleDiv);
-    chatMessages.appendChild(messageDiv);
+    chatMessages.appendChild(messageDiv)
 
     scrollToBottom();
+}
+
+// 로딩
+function showLoading() {
+    const loadingDiv = document.createElement('div');
+    loadingDiv.id = 'loading';
+    loadingDiv.className = 'message bot';
+
+    const logoImg = document.createElement('img');
+    logoImg.src = 'bot-logo.png';
+    logoImg.className = 'bot-logo';
+
+    const bubbleDiv = document.createElement('div');
+    bubbleDiv.className = 'bubble typing';
+    bubbleDiv.textContent = '…';
+
+    loadingDiv.appendChild(logoImg);
+    loadingDiv.appendChild(bubbleDiv);
+
+    chatMessages.appendChild(loadingDiv);
+    scrollToBottom();
+}
+
+
+function hideLoading() {
+    const loadingDiv = document.getElementById('loading');
+    if (loadingDiv) chatMessages.removeChild(loadingDiv);
 }
 
 // 항상 스크롤 하단 유지
@@ -22,19 +56,37 @@ function scrollToBottom() {
     chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
+
+
 //전송 버튼 클릭
 sendBtn.addEventListener('click', () => {
     const text = input.value.trim();
     if (!text) return;
 
-    renderMessage(text, 'user'); // 사용자 메시지
+    renderMessage(text, 'user');
     input.value = ''; // 입력창 초기화
 
-    // 테스트용 메시지
-    renderMessage('답변입니다.', 'bot');
+    // 답변
+    handleBotReply(text);
 });
+
+// test용 답변
+function handleBotReply(userText) {
+    sendBtn.disabled = true; //중복 방지
+    showLoading();
+
+    // 추후 수정 예정
+    setTimeout(() => {
+        hideLoading();
+        renderMessage('test 봇 답변입니다.', 'bot');
+        sendBtn.disabled = false; //중복 방지 해제
+    }, 1000);
+}
+
 
 // Enter 적용
 input.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') sendBtn.click();
+    if (e.key === 'Enter' && !sendBtn.disabled) {
+        sendBtn.click();
+    }
 });
