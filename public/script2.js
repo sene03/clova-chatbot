9f119a0de15a8555d6ec52fd966843801ca0d888
hyun7931 <shlee4185@gmail.com>
2026-01-07 16:35:16 +0900

    feat : script2 작업중

--- public/script2.js
+++ public/script2.js
@@ -106,12 +106,21 @@ document.addEventListener("DOMContentLoaded", () => {
         if (loadingDiv) loadingDiv.remove();
     }
 
-    async function sendMessage() {
+    async function sendMessage(){
         const text = userInput.value.trim();
         if (!text) return;
 
         renderMessage(text, "user");
         userInput.value = "";
+
+        await askServer(text);
+    }
+
+    async function sendPayload(payload){
+        await askServer(payload);
+    }
+
+    async function askServer(msgContent) {
 
         toggleInput(true);
         showLoading();
@@ -122,7 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
                     userId: currentUserId,
-                    message: text,
+                    message: msgContent,
                 }),
             });
 
@@ -134,7 +143,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     renderMessage(msg, 'bot');
                 });
             }
-
+            
+            //TODO : renderButtons
             if(data.choices && Array.isArray(data.choices)){
                 data.choices.forEach(msg => {
                     renderMessage(msg.label, 'bot');
