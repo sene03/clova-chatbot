b4c690b0756e159ba6a3b3af00d480b90f92f83d
sene03 <hansel0311@gmail.com>
2026-01-07 09:48:35 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// express server
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: "." });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
